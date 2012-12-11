6e63c647a9246bdbe6e63fb7a76e303dc25617c1
cadorn <christoph@christophdorn.com>
2012-12-10 20:06:28 -0800

    impl

--- plugin.js
+++ plugin.js
@@ -1,6 +1,11 @@
 
-
 exports.for = function(API, plugin) {
 
+    plugin.extract = function(fromPath, toPath, locator, options) {
+    	// TODO: If `unzip` command not found use pure JS implementation.
+		return API.OS.exec("unzip '" + fromPath + "' -d '" + toPath + "'").then(function() {
+			return 200;
+		});
+	}
 }
 
